6b0812b3f654410a78d77f15087757f4cb42ff20
sujin16 <tnwls9712@ajou.ac.kr>
2019-09-26 00:02:23 +0900

    Update sdk version

--- app/src/main/java/com/jieun/cardiocare/MainActivity.java
+++ app/src/main/java/com/jieun/cardiocare/MainActivity.java
@@ -1,20 +1,18 @@
 package com.jieun.cardiocare;
-
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
-
+import android.R.*;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -29,8 +27,6 @@ import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.facebook.login.widget.LoginButton;
-
 
 public class MainActivity extends AppCompatActivity {
 
